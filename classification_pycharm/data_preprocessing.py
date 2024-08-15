db3e9e707d5fd9ad3c06f9b58016989e2e363368
lumine-1 <scyyh123@gmail.com>
2024-08-15 18:08:23 +0100

    First upload of project files

---
+++ classification_pycharm/data_preprocessing.py
@@ -0,0 +1,117 @@
+import os
+
+import cv2
+import numpy as np
+import pandas as pd
+from collections import Counter
+from PIL import Image
+from utils import apply_random_transform_and_save
+import torchvision.transforms as transforms
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+
+# Read the training set label and image path
+def load_data(labels_csv, images_dir, validation_labels_csv, validation_images_dir):
+    labels_df = pd.read_csv(labels_csv)
+    file_paths = [os.path.join(images_dir, fname) for fname in labels_df['image']]
+    # read the label
+    labels = labels_df['myopic_maculopathy_grade'].values
+
+    # Read the validation set label and image path
+    validation_labels_df = pd.read_csv(validation_labels_csv)
+    validation_file_paths = [os.path.join(validation_images_dir, fname) for fname in validation_labels_df['image']]
+    # read the label
+    validation_labels = validation_labels_df['myopic_maculopathy_grade'].values
+
+    return file_paths, labels, validation_file_paths, validation_labels
+
+
+# this is pre-training data augmentation
+def augment_data(file_paths, labels):
+    enhanced_file_paths = []
+    enhanced_labels = []
+
+    # define data enhancement transformations
+    target_transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        # is it inconsistent with reality?
+        transforms.RandomRotation(10),
+        transforms.Resize(224),
+        transforms.ColorJitter(brightness=0.3, contrast=0.3),
+    ])
+
+    # define data enhancement transformations using albumentaions library
+    transform_train = A.Compose([
+        # p is probability of using certain transformation
+        A.Flip(always_apply=False, p=0.5),
+        A.ShiftScaleRotate(shift_limit=0.2, scale_limit=0.1, rotate_limit=45, p=0.5),
+        A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=1.0),
+        A.RandomGamma(gamma_limit=(80, 120), p=1.0),
+        A.Resize(height=224, width=224, p=1.0),
+        A.Normalize(mean=(0, 0, 0), std=(1, 1, 1), p=1.0),
+        # to tensor
+        ToTensorV2(),
+    ], p=1)
+
+    for path, label in zip(file_paths, labels):
+        enhanced_file_paths.append(path)
+        enhanced_labels.append(label)
+        # if label in [0]:  # add class 0 sample
+        #     for _ in range(1):  # times of repetition
+        #         transformed_path = apply_random_transform_and_save(path, transform_train)
+        #         enhanced_file_paths.append(transformed_path)
+        #         enhanced_labels.append(label)
+        # if label in [1]:  # add class 1 sample
+        #     for _ in range(1):  # times of repetition
+        #         transformed_path = apply_random_transform_and_save(path, transform_train)
+        #         enhanced_file_paths.append(transformed_path)
+        #         enhanced_labels.append(label)
+        if label in [2]:  # add class 2 sample
+            for _ in range(1):  # times of repetition
+                transformed_path = apply_random_transform_and_save(path, transform_train)
+                enhanced_file_paths.append(transformed_path)
+                enhanced_labels.append(label)
+        if label in [3]:  # add class 3 sample
+            for _ in range(5):  # times of repetition
+                transformed_path = apply_random_transform_and_save(path, transform_train)
+                enhanced_file_paths.append(transformed_path)
+                enhanced_labels.append(label)
+        if label in [4]:  # add class 4 sample
+            for _ in range(7):  # times of repetition
+                transformed_path = apply_random_transform_and_save(path, transform_train)
+                enhanced_file_paths.append(transformed_path)
+                enhanced_labels.append(label)
+
+    return enhanced_file_paths, enhanced_labels
+
+
+# data preprocessing functions
+# use CLAHE to preprocess images
+def apply_clahe_to_image(image):
+    image_np = np.array(image)
+    lab = cv2.cvtColor(image_np, cv2.COLOR_RGB2LAB)
+    # separate LAB channels
+    l, a, b = cv2.split(lab)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    l_clahe = clahe.apply(l)
+    # combine channels
+    lab_clahe = cv2.merge((l_clahe, a, b))
+    image_np_clahe = cv2.cvtColor(lab_clahe, cv2.COLOR_LAB2RGB)
+    # convert NumPy array to PIL images
+    image_clahe = Image.fromarray(image_np_clahe)
+
+    return image_clahe
+
+
+# use load_ben_color (this is not used as it is not useful)
+def load_ben_color(path):
+    # read images
+    image = cv2.imread(path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (224, 224))
+    # sharpened the image
+    # the input image is processed by Gaussian blur, elevates the edges of the original image
+    image = cv2.addWeighted(image, 4, cv2.GaussianBlur(image, (6, 6), 8), -4, 128)
+
+    return image
---
+++ classification_pycharm/nested_test.py
@@ -0,0 +1,309 @@
+import copy
+import functools
+
+import torch
+import utils
+import optuna
+import numpy as np
+from sklearn.model_selection import ParameterGrid, StratifiedKFold, ParameterSampler
+from sklearn.metrics import confusion_matrix, cohen_kappa_score, f1_score
+from torch import nn
+from torch.utils.data import DataLoader, Subset, default_collate
+from torchvision.transforms import transforms
+from tqdm import tqdm
+from model import ConvNeXtModel, GoogleModel
+from dataset import MyDataset, AugmentedDataset
+from optuna.integration import PyTorchLightningPruningCallback
+
+# this is the third way of training models, using corss validation to test the model
+# use 4-fold corss validation here
+# also use
+def nested_train(model, paths, labels, criterion, optimizer, num_epochs=10, k_outer=4, k_inner=4):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # divide the dataset, use StratifiedKFold to make sure the data distribution is similar
+    outer_kfold = StratifiedKFold(n_splits=k_outer, shuffle=True, random_state=42)
+    inner_kfold = StratifiedKFold(n_splits=k_inner, shuffle=True, random_state=42)
+
+    dataset = MyDataset(paths, labels, transform=transform)
+
+    # set hyperparameters
+    # (due to the computation power, not able to tune too many parameters)
+    param = {
+        'lr': np.logspace(-6, -2, 100),
+        'epoch': [5, 10, 15, 20, 25]
+    }
+    n_iter = 10
+    param_sampler = ParameterSampler(param, n_iter=n_iter)
+
+    for outer_fold, (outer_train_idx, outer_test_idx) in enumerate(outer_kfold.split(range(len(dataset)), labels)):
+        outer_train_dataset = Subset(dataset, outer_train_idx)
+        outer_test_dataset = Subset(dataset, outer_test_idx)
+
+        best_avg = -np.inf
+        best_params = []
+
+        for params in param_sampler:
+            current_avg_scores = []
+            for inner_fold, (inner_train_idx, inner_val_idx) in enumerate(inner_kfold.split(range(len(outer_train_dataset)), [labels[i] for i in outer_train_idx])):
+
+                # no data augmentation
+                # inner_val = DataLoader(Subset(outer_train_dataset, inner_val_idx), batch_size=16)
+                # inner_train = DataLoader(Subset(outer_train_dataset, inner_train_idx), batch_size=16, shuffle=True)
+
+                # define the label that needs to be enhanced and how many times it should be enhanced
+                training_set = Subset(outer_train_dataset, inner_train_idx)
+                augmented_images, augmented_labels = augment_dataset(training_set, train_transform)
+                augmented_dataset = AugmentedDataset(augmented_images, augmented_labels)
+                inner_train = DataLoader(augmented_dataset, batch_size=16, shuffle=True)
+                inner_val = DataLoader(Subset(outer_train_dataset, inner_val_idx), batch_size=16)
+
+                model = ConvNeXtModel().to(device)
+                optimizer = torch.optim.AdamW(model.parameters(), lr=params['lr'], weight_decay=1e-4)
+
+                for epoch in range(params['epoch']):
+                    train_epoch(model, inner_train, criterion, optimizer, device)
+
+                all_labels, all_preds = validate_epoch(model, inner_val, criterion, device)
+
+                cm = confusion_matrix(all_labels, all_preds)
+                qwk = cohen_kappa_score(all_labels, all_preds, weights='quadratic')
+                macro_f1 = f1_score(all_labels, all_preds, average='macro')
+                classes = np.unique(all_labels)
+                macro_specificity = utils.calculate_specificity(np.array(all_labels), np.array(all_preds), classes)
+                avg = (macro_f1 + macro_specificity + qwk) / 3
+                current_avg_scores.append(avg)
+                print(params)
+                print(f"Confusion Matrix:\n{cm}")
+                print(f"Average Score: {avg:.4f}")
+                if avg < 0.78:
+                    print("too small, break")
+                    break
+
+            # calculates the average performance under the current parameter configuration
+            current_params_avg = np.mean(current_avg_scores)
+            print(f"Total Average Score: {current_params_avg:.4f}")
+
+            # save the best result
+            if current_params_avg > best_avg:
+                best_avg = current_params_avg
+                best_params = params
+
+        print("Best Parameters:", best_params)
+
+        # Evaluation on the outer test set
+        test_loader = torch.utils.data.DataLoader(outer_test_dataset, batch_size=16, shuffle=False)
+        model = ConvNeXtModel().to(device)  # use new ConvNeXtModel
+        optimizer = torch.optim.AdamW(model.parameters(), lr=best_params['lr'], weight_decay=1e-4)
+
+        # retrain the model to use the best parameters
+        for epoch in range(best_params['epoch']):  # use best_params
+            train_epoch(model, DataLoader(outer_train_dataset, batch_size=16, shuffle=True), criterion, optimizer,
+                        device)
+        all_labels, all_preds = validate_epoch(model, test_loader, criterion, device)
+
+        # print the result
+        test_cm = confusion_matrix(all_labels, all_preds)
+        test_qwk = cohen_kappa_score(all_labels, all_preds, weights='quadratic')
+        test_macro_f1 = f1_score(all_labels, all_preds, average='macro')
+        test_macro_specificity = utils.calculate_specificity(np.array(all_labels), np.array(all_preds),
+                                                             np.unique(all_labels))
+        test_avg = (test_macro_f1 + test_macro_specificity + test_qwk) / 3
+
+        print("////////////////////////////////////////////////////////////////////////")
+        print(f"Test Metrics with Best Parameters:")
+        print(f"Confusion Matrix:\n{test_cm}")
+        print(f"Quadratic Weighted Kappa: {test_qwk:.4f}")
+        print(f"Macro F1-Score: {test_macro_f1:.4f}")
+        print(f"Macro Specificity: {test_macro_specificity:.4f}")
+        print(f"Average Score: {test_avg:.4f}")
+        print("////////////////////////////////////////////////////////////////////////")
+
+# simplify the train function
+def train_epoch(model, data_loader, criterion, optimizer, device):
+    model.train()
+    running_loss = 0.0
+    for inputs, labels in tqdm(data_loader, desc="Training"):
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+
+# simplify the train function
+def validate_epoch(model, data_loader, criterion, device):
+    model.eval()
+    all_preds = []
+    all_labels = []
+    with torch.no_grad():
+        for inputs, labels, paths in tqdm(data_loader, desc="Validation"):
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            all_preds.extend(preds.cpu().numpy())
+            all_labels.extend(labels.cpu().numpy())
+    return all_labels, all_preds
+
+
+# also define preprocessing transform
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+
+# also define preprocessing transform for training set
+train_transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(10),
+    transforms.Resize(224),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2),
+])
+
+
+def augment_dataset(dataset, transform):
+    augmented_images = []
+    augmented_labels = []
+
+    for i in range(len(dataset)):
+        img, label, path = dataset[i]
+
+        # add original image
+        augmented_images.append(img)
+        augmented_labels.append(label)
+
+        # if label in [0]:
+        #     for _ in range(1):
+        #         augmented_img = transform(img)
+        #         augmented_images.append(augmented_img)
+        #         augmented_labels.append(label)
+        # if label in [1]:
+        #     for _ in range(1):
+        #         augmented_img = transform(img)
+        #         augmented_images.append(augmented_img)
+        #         augmented_labels.append(label)
+        if label in [2]:
+            for _ in range(1):
+                augmented_img = transform(img)
+                augmented_images.append(augmented_img)
+                augmented_labels.append(label)
+        if label in [3]:
+            for _ in range(5):
+                augmented_img = transform(img)
+                augmented_images.append(augmented_img)
+                augmented_labels.append(label)
+        if label in [4]:
+            for _ in range(7):
+                augmented_img = transform(img)
+                augmented_images.append(augmented_img)
+                augmented_labels.append(label)
+
+    return augmented_images, augmented_labels
+
+# this functino is to create an optuna parameter search work
+# very time-consuming (not give the best result)
+def objective(trial, labels, outer_train_dataset, outer_train_idx):
+    # define the hyperparameter space
+    lr = trial.suggest_float('lr', 1e-6, 1e-2, log=True)
+    epochs = trial.suggest_int('epochs', 1, 50)
+    criterion = nn.CrossEntropyLoss()
+    scores = []
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    inner_kfold = StratifiedKFold(n_splits=4, shuffle=True, random_state=42)
+    for inner_fold, (inner_train_idx, inner_val_idx) in enumerate(
+            inner_kfold.split(range(len(outer_train_dataset)), [labels[i] for i in outer_train_idx])):
+
+        # use divided data
+        training_set = Subset(outer_train_dataset, inner_train_idx)
+        augmented_images, augmented_labels = augment_dataset(training_set, train_transform)
+        augmented_dataset = AugmentedDataset(augmented_images, augmented_labels)
+        inner_train = DataLoader(augmented_dataset, batch_size=16, shuffle=True)
+        inner_val = DataLoader(Subset(outer_train_dataset, inner_val_idx), batch_size=16)
+
+        # use a new model (avoid using the same model for different trails)
+        model = ConvNeXtModel().to(device)
+        optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-4)
+
+        for epoch in range(epochs):
+            train_epoch(model, inner_train, criterion, optimizer, device)
+        all_labels, all_preds = validate_epoch(model, inner_val, criterion, device)
+
+        # evaluate
+        cm = confusion_matrix(all_labels, all_preds)
+        qwk = cohen_kappa_score(all_labels, all_preds, weights='quadratic')
+        macro_f1 = f1_score(all_labels, all_preds, average='macro')
+        classes = np.unique(all_labels)
+        macro_specificity = utils.calculate_specificity(np.array(all_labels), np.array(all_preds), classes)
+        avg = (macro_f1 + macro_specificity + qwk) / 3
+        scores.append(avg)
+        print(lr)
+        print(epochs)
+        print(f"Confusion Matrix:\n{cm}")
+        print(f"Average Score: {avg:.4f}")
+
+    average_score = sum(scores) / len(scores)
+    print(f"Final Score: {average_score:.4f}")
+    return average_score
+
+
+# train using optuna (this is too time-consuming, did not give the best answer)
+def optuna_train(paths, labels, criterion):
+    # create dataset instance
+    dataset = MyDataset(paths, labels, transform=transform)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # process for training and validation
+    outer_kfold = StratifiedKFold(n_splits=4, shuffle=True, random_state=42)
+    for outer_fold, (outer_train_idx, outer_test_idx) in enumerate(outer_kfold.split(range(len(dataset)), labels)):
+        outer_train_dataset = Subset(dataset, outer_train_idx)
+        outer_test_dataset = Subset(dataset, outer_test_idx)
+
+        augmented_images, augmented_labels = augment_dataset(outer_train_dataset, train_transform)
+        augmented_dataset = AugmentedDataset(augmented_images, augmented_labels)
+        outer_train = DataLoader(augmented_dataset, batch_size=16, shuffle=True)
+        outer_test = DataLoader(outer_test_dataset, batch_size=16)
+
+        # partial_objective = functools.partial(objective, labels=labels, outer_train_dataset=outer_train_dataset,
+        #                                       outer_train_idx=outer_train_idx)
+        # we need the result to be larger
+        # study = optuna.create_study(direction='maximize')
+        # study.optimize(partial_objective, n_trials=12)
+        # utils.visualise_optuna(study)
+        #
+        # choose 3 best trail (because I want to test not only the best result, to prove it can )
+        # sorted_trials = sorted(study.trials, key=lambda trial: trial.value, reverse=True)
+        # best_three_trials = sorted_trials[:3]
+        # print(best_three_trials)
+        #
+
+        # this is used to test the best settings found, can be deleted
+        # best_params = study.best_trial.params
+        best_params = {'lr': 2.7795834117065912e-05, 'epochs': 38}
+
+        model = ConvNeXtModel().to(device)
+        optimizer = torch.optim.AdamW(model.parameters(), lr=best_params['lr'], weight_decay=1e-4)
+
+        for epoch in range(best_params['epochs']):
+            train_epoch(model, outer_train, criterion, optimizer, device)
+        all_labels, all_preds = validate_epoch(model, outer_test, criterion, device)
+
+        # evaluate
+        cm = confusion_matrix(all_labels, all_preds)
+        qwk = cohen_kappa_score(all_labels, all_preds, weights='quadratic')
+        macro_f1 = f1_score(all_labels, all_preds, average='macro')
+        classes = np.unique(all_labels)
+        macro_specificity = utils.calculate_specificity(np.array(all_labels), np.array(all_preds), classes)
+        avg = (macro_f1 + macro_specificity + qwk) / 3
+        print(best_params['lr'])
+        print(best_params['epochs'])
+        print(f"Confusion Matrix:\n{cm}")
+        print(f"Average Score: {avg:.4f}")
+        print("ok, good, finish!!")
+        break
+
+
+
